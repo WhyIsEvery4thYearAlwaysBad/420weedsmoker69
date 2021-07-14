48bcc1ec8c9c0069f6c5dcbe43c5ad44029d2c6e
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-07-14 19:36:16 -0400

    CMenu generation script now automatically sets the bind counts.
    For the bind at line 512 in 420base.cmenu.

--- cmenus/generate_cmenu.sh
+++ cmenus/generate_cmenu.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# POSIX compliance baby!
+# Combine the three cmenus into one large cmenu.
 cat ./420base.cmenu > ../420cmdmenu.cmenu
 ./generate_call_cmenu.sh >> ../420cmdmenu.cmenu
 cat ./navmesheditor.cmenu >> ../420cmdmenu.cmenu
+# Set the bind counts for the bind at line 512 in 420base.cmenu.
+BIND_COUNT=$(grep -c -i "BIND\([[:space:]]*\)\"\(.*\)\"[[:space:]]*\"\(.\{1,\}\)\"" ../420cmdmenu.cmenu)
+CHAT_BIND_COUNT=$(grep -c -i "BIND\([[:space:]]*\)\"\(.*\)\"[[:space:]]*\"say\(_team\)\?[[:space:]]\(.\{1,\}\)\"" ../420cmdmenu.cmenu)
+sed -i "s/{_BIND_COUNT_}/${BIND_COUNT}/" ../420cmdmenu.cmenu
+sed -i "s/{_CHAT_BIND_COUNT_}/${CHAT_BIND_COUNT}/" ../420cmdmenu.cmenu
