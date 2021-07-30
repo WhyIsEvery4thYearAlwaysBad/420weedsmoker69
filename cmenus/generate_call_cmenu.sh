94c3919cee05da2a5da18028fa5a50952b2ef6bc
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-07-30 12:09:11 -0400

    Add more weapons.

--- cmenus/generate_call_cmenu.sh
+++ cmenus/generate_call_cmenu.sh
@@ -3,7 +3,35 @@
 # Config
 TITLE="[Calls]"
 ClassNames=("scout" "solly" "pyro" "demo" "heavy" "engi" "medic" "sniper" "spy" "medic Combo" "trap" 'sentry dispenser tele nest')
-Weapons=( 'bonk! crit-A-Cola Mad\040Milk' 'Buff\040Banner battals conch gunboats whip'  'phlog Gas\040Passer' 'shield scoRes' 'foS GRU' 'rescue\040Ranger frontier\040Justice wrangler short\040Circuit eureka\040Effect jag' 'Stock\040Medigun kritz quickie vacc Solemn\040Vow' 'jarate' 'yer')
+Weapons=( 'scattergun fan shortstop soda\040popper bfb back\040scatter' \
+'pistol winger pbpp cleaver milk bonk caC'  \
+'bat sand assassin cane bash stick fow atom' \
+'stock\040rl origin direct black\040box rjumper liberty mangler beggars striker' \
+'shotgun reserve buff battals conch gunboats treads bison bjumper panic' \
+'shovel i-pick capper anime-sword whip marketeer d-pick' \
+'flamethrower backburner degreaser phlog dragons' \
+'shotgun panic reserve flare detonator melter scorch gas' \
+'stock\040axe axtinguisher hammer powerjack back\040scratcher fragment 3° neon hand' \
+'grenade' 'loch' 'bomber' 'boots' 'jumper' \
+'stick-launcher scores quickiebomb sjumper bjumper' \
+'bottle eyelander cutter capper caber claid anime-sword persian' \
+'minigun natascha brass tomislav heater' \
+'shotgun panic family sandvich banana snickers steak' \
+'fists crit-gloves gru warrior fos evictor holipunch' \
+'shotgun panic frontier widowmaker pomp res.\040ranger' \
+'pistol wrangler short\040circuit' \
+'wrench gunslinger eureka jag southern' \
+'crossbow blut overdose stock\040syringe' \
+'medigun kritz quickie vacc' \
+'übersaw vitasaw vow amp bonesaw' \
+'sniper bow sydney bazaar machina heatmaker classic' \
+'smg cleaners jarate razorback fire-shield camper' \
+'kukuri bushwacka shahanshah shiv' \
+'revolver diamondback letranger ambassador enforcer' \
+'knife kunai earner spycicle' \
+'stock\040watch cad ringer sapper rtr' \
+)
+declare -a ScoutPrimaries=('scattergun' 'fan')
 # end Config
 
 # Function to generate position calls. I know this function can be optimized to not use 19 for loops, but that's too much work.
@@ -289,20 +317,55 @@ main() {
 			if [ ! ${#Weapons[ClassID+SubID]} -eq 0 ]
 			then
 				echo -e "\t\t\t\"[Weapons]\" {"
-				for WeaponGroup in ${Weapons[ClassID+SubID]}
+				# Primary Weapons
+				echo -e "\t\t\t\t\"[Primary]\" {"
+				for PrimaryWeaponGroup in ${Weapons[$(((ClassID+SubID)*3))]}
 				do
-					for Weapon in ${WeaponGroup[@]}
+					for Weapon in ${PrimaryWeaponGroup[@]}
 					do
-						echo -e "\t\t\t\t\"[${Weapon^}]\" {"
-						echo -e "\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team ENEMY ${ClassNames[ClassID+SubID]^^} USE ${Weapon^^} (BIND)\" |"
+						echo -e "\t\t\t\t\t\"[${Weapon^}]\" {"
+						echo -e "\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team ENEMY ${ClassNames[ClassID+SubID]^^} USE ${Weapon^^} (BIND)\" |"
 						for Amount in {2..9} 0
 						do
-							echo -e "\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${ClassNames[ClassID+SubID]^^}S USE ${Weapon^^} (BIND)\" |"
+							echo -e "\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${ClassNames[ClassID+SubID]^^}S USE ${Weapon^^} (BIND)\" |"
 						done
-						echo -e "\t\t\t\t}"
+						echo -e "\t\t\t\t\t}"
 					done
 				done
-			# End Weapon calls
+				echo -e "\t\t\t\t}"
+				# Secondary Weapons
+				echo -e "\t\t\t\t\"[Secondary]\" {"
+				for SecondWeaponGroup in ${Weapons[$(((ClassID+SubID)*3+1))]}
+				do
+					for Weapon in ${SecondWeaponGroup[@]}
+					do
+						echo -e "\t\t\t\t\t\"[${Weapon^}]\" {"
+						echo -e "\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team ENEMY ${ClassNames[ClassID+SubID]^^} USE ${Weapon^^} (BIND)\" |"
+						for Amount in {2..9} 0
+						do
+							echo -e "\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${ClassNames[ClassID+SubID]^^}S USE ${Weapon^^} (BIND)\" |"
+						done
+						echo -e "\t\t\t\t\t}"
+					done
+				done
+				echo -e "\t\t\t\t}"
+				# Tertiary Weapons
+				echo -e "\t\t\t\t\"[Tertiary]\" {"
+				for TertiaryWeaponGroup in ${Weapons[$(((ClassID+SubID)*3+2))]}
+				do
+					for Weapon in ${TertiaryWeaponGroup[@]}
+					do
+						echo -e "\t\t\t\t\t\"[${Weapon^}]\" {"
+						echo -e "\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team ENEMY ${ClassNames[ClassID+SubID]^^} USE ${Weapon^^} (BIND)\" |"
+						for Amount in {2..9} 0
+						do
+							echo -e "\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${ClassNames[ClassID+SubID]^^}S USE ${Weapon^^} (BIND)\" |"
+						done
+						echo -e "\t\t\t\t\t}"
+					done
+				done
+				echo -e "\t\t\t\t}"
+				# End Weapon calls
 				echo -e "\t\t\t}"
 			fi
 			# Start Spy Disguises.
@@ -367,7 +430,7 @@ main() {
 				for Banner in "Buff\040Banner" "Battal" "Conch"
 				do
 					echo -e "\t\t\t\t\"[${Banner}]\" {"
-					echo -e "\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team ENEMY ${Banner^^} PREPPED (BIND)\" |"
+					echo -e "\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>_\" \"say_team 1 ENEMY ${Banner^^} PREPPED (BIND)\" |"
 					for Num in {2..9} 0
 					do
 						echo -e "\t\t\t\t\tNOFORMAT BIND \"\" \"say_team $Num ENEMY ${Banner^^}S PREPPED (BIND)\" |"
@@ -381,72 +444,53 @@ main() {
 			if [ $ClassID -eq 6 ] && [ $SubID -eq 0 ]
 			then
 				echo -e "\t\t\t\"[Über]\" {"
-				for UberType in "stock" "kritz" "quickie" "vacc"
+				for UberType in "stock" "kritz" "quickie" "vacc" "unknown"
 				do
 					echo -e "\t\t\t\t\"[${UberType^}]\" {"
 					echo -e "\t\t\t\t\t\"[Value]\" {"
-					for Digit1 in {1..9} 0
+					echo -e "\t\t\t\t\t\t\"[10-30%]\" {"
+					for UberValue in {1..3}0
 					do
-						if [ $Digit1 -eq 1 ]
-						then
-							echo -e "\t\t\t\t\t\tNOFORMAT \"<clr:234,209,174>_\" {"
-						else
-							echo -e "\t\t\t\t\t\tNOFORMAT \" \" {"
-						fi
-						for Digit2 in {1..9} 0
+						echo -e "\t\t\t\t\t\t\t\"$UberValue%\" {"
+						echo -e "\t\t\t\t\t\t\t\tBIND \"<clr:234,209,174>Amount: _\" \"say_team 1 ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
+						for Amount in {2..9} 0
 						do
-							if [ $Digit2 -eq 1 ]
-							then
-								if [ $Digit1 -eq 0 ]
-								then
-									echo -e "\t\t\t\t\t\t\tNOFORMAT \"<clr:234,209,174>_\" {"
-								else
-									echo -e "\t\t\t\t\t\t\tNOFORMAT \"<clr:234,209,174>${Digit1}_\" {"
-								fi
-							else
-								echo -e "\t\t\t\t\t\t\tNOFORMAT \" \" {"
-							fi
-							if [ $Digit1 -eq 0 ]
-							then
-								echo -e "\t\t\t\t\t\t\t\tKEY = \"ENTER\" NOFORMAT \"<clr:234,209,174>${Digit2}_\" {"
-								for Amount in {1..9} 0
-								do
-									if [ $Amount -eq 1 ]
-									then
-										echo -e "\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>Amount: _\" \"say_team ENEMY ${UberType^^} ÜBER ${Digit2}% (BIND)\" |"
-									else
-										echo -e "\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${UberType^^} ÜBERS ${Digit2}% (BIND)\" |"
-									fi
-								done
-								echo -e "\t\t\t\t\t\t\t\t}"
-							else
-								echo -e "\t\t\t\t\t\t\t\tKEY = \"ENTER\" NOFORMAT \"<clr:234,209,174>${Digit1}${Digit2}_\" {"
-								echo -e "\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>Amount: _\" \"say_team ENEMY ${UberType^^} ÜBER ${Digit1}${Digit2}% (BIND)\" |"
-								for Amount in {2..9} 0
-								do
-									echo -e "\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${UberType^^} ÜBERS ${Digit1}${Digit2}% (BIND)\" |"
-								done
-								echo -e "\t\t\t\t\t\t\t\t}"
-							fi
-							# Make 100% über bind
-							if [ $Digit1 -eq 1 ] && [ $Digit2 -eq 0 ]
-							then
-								echo -e "\t\t\t\t\t\t\t\tKEY = \"0\" NOFORMAT \" \" {"
-								echo -e "\t\t\t\t\t\t\t\t\tKEY = \"ENTER\" NOFORMAT \"<clr:234,209,174>${Digit1}${Digit2}0_\" {"
-								echo -e "\t\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"<clr:234,209,174>Amount: _\" \"say_team ENEMY ${UberType^^} ÜBER 100% (BIND)\" |"
-								for Amount in {2..9} 0
-								do
-									echo -e "\t\t\t\t\t\t\t\t\t\tNOFORMAT BIND \"\" \"say_team ${Amount} ENEMY ${UberType^^} ÜBERS 100% (BIND)\" |"
-								done
-								{
-									echo -e "\t\t\t\t\t\t\t\t\t}"
-									echo -e "\t\t\t\t\t\t\t\t}"
-								}
-							fi
-							echo -e "\t\t\t\t\t\t\t}"
+							echo -e "\t\t\t\t\t\t\t\tBIND \"\" \"say_team $Amount ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
 						done
-						echo -e "\t\t\t\t\t\t}"
+						echo -e "\t\t\t\t\t\t\t}"
 					done
+					echo -e "\t\t\t\t\t\t}"
+					echo -e "\t\t\t\t\t\t\"[40-60%]\" {"
+					for UberValue in {4..6}0
+					do
+						echo -e "\t\t\t\t\t\t\t\"$UberValue%\" {"
+						echo -e "\t\t\t\t\t\t\tBIND \"<clr:234,209,174>Amount: _\" \"say_team 1 ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
+						for Amount in {2..9} 0
+						do
+							echo -e "\t\t\t\t\t\t\tBIND \"\" \"say_team $Amount ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
+						done
+						echo -e "\t\t\t\t\t\t\t}"
+					done
+					echo -e "\t\t\t\t\t\t}"
+					echo -e "\t\t\t\t\t\t\"[70-90%]\" {"
+					for UberValue in {7..9}0
+					do
+						echo -e "\t\t\t\t\t\t\t\"$UberValue%\" {"
+						echo -e "\t\t\t\t\t\t\t\tBIND \"$UberValue%\" \"say_team 1 ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
+						for Amount in {2..9} 0
+						do
+							echo -e "\t\t\t\t\t\t\tBIND \"$UberValue%\" \"say_team $Amount ENEMY ${UberType^^} ÜBERS $UberValue% (BIND)\" |"
+						done
+						echo -e "\t\t\t\t\t\t\t}"
+					done
+					echo -e "\t\t\t\t\t\t}"
+					echo -e "\t\t\t\t\t\t\"[100%]\" {"
+					echo -e "\t\t\t\t\t\t\tBIND \"<clr:234,209,174>Amount: _\" \"say_team 1 ENEMY ${UberType^^} ÜBERS 100% (BIND)\" |"
+					for Amount in {2..9} 0
+					do
+						echo -e "\t\t\t\t\t\t\tBIND \"\" \"say_team $Amount ENEMY ${UberType^^} ÜBERS 100% (BIND)\" |"
+					done
+					echo -e "\t\t\t\t\t\t}"
 					echo -e "\t\t\t\t\t}"
 					for Verb in 'popped' 'dropped' 'faked'
 					do
