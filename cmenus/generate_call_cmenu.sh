c7718076c78daffbc17a548c3126527ecb6226d7
Amicdict <akalaamibro@protonmail.com>
2021-12-17 03:44:26 -0500

    Perform stenography on call binds.

--- cmenus/generate_call_cmenu.sh
+++ cmenus/generate_call_cmenu.sh
@@ -39,14 +39,14 @@ gen_detailed_pos_calls() {
 		if [ "$Point" = "BLU Spawn" ]
 		then
 			printf "%b\t\"[Spawn]\" {\n" "${TAB_STR}"
-			./gen_pos_calls.sh "$1 $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "\"[${Point%" Spawn "}]\" {"
+			./gen_pos_calls.sh "$1 @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "\"[${Point%" Spawn "}]\" {"
 		elif [ "$Point" = "RED Spawn" ]
 		then
-			./gen_pos_calls.sh "$1 $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "\"[${Point%" Spawn "}]\" {"
+			./gen_pos_calls.sh "$1 @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "\"[${Point%" Spawn "}]\" {"
 			printf "%b\t}\n" "${TAB_STR}"
 			printf "%b\t\"[Point]\" {\n" "${TAB_STR}"
 		else 
-			./gen_pos_calls.sh "$1 $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "NOFORMAT \" \" {"
+			./gen_pos_calls.sh "$1 @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${2} @ $(printf "%s" "$Point" | tr "[:lower:]" "[:upper:]")" "${TAB_STR}\t\t" "NOFORMAT \" \" {"
 		fi
 	done
 	printf "%b\t}\n" "${TAB_STR}"
@@ -58,39 +58,16 @@ gen_detailed_pos_calls() {
 main() {
 	printf '"%s" {\n' "$TITLE"
 	# Get all classes
-	for ClassGroup in 'scout soldier pyro' 'demo heavy engi' 'medic sniper spy' 'med-combo trap builds'
+	for ClassGroup in 'Scout Soly Pyro' 'Demo Heavy Engi' 'Medic Snipr Spy' 'Med-Combo Trap Builds'
 	do
 		# Name the building array "Builds" instead of 'sentry dispenser…'.
 		printf '\t"[%s]" {\n' "$(printf "$ClassGroup" | tr '[:space:]' '/')"
-
 		# The actual calls
 		for Class in $ClassGroup
 		do
-		    # Dont let the classid go beyond the allowed limit
-		    # Generate position calls for all classes. Not buildings yet tho.
-		      	if [ "$Class" = "med-combo" ]
-		 	then
-		 		printf '\t\t"[Medic Combo]" {\n'
-		 		printf '\t\t\t"[Scou/Solly/Pyr]" {\n'
-		 		gen_detailed_pos_calls "1 SCOUT-MED" "SCOUT-MEDs" "\t\t\t\t" "\"[Scou]\" {"
-		 		gen_detailed_pos_calls "1 SOLLY-MED" "SOLLY-MEDs" "\t\t\t\t" "\"[Solly]\" {"
-		 		gen_detailed_pos_calls "1 PYRO-MED" "PYRO-MEDs" "\t\t\t\t" "\"[Pyr]\" {"
-		 		printf '\t\t\t}\n'
-		 		printf '\t\t\t"[Dem/Heav/Eng]" {\n'
-		 		gen_detailed_pos_calls "1 DEMO-MED" "DEMO-MEDs" "\t\t\t\t" "\"[Dem]\" {"
-		 		gen_detailed_pos_calls "1 HEAVY-MED" "HEAVY-MEDs" "\t\t\t\t" "\"[Heav]\" {"
-		 		gen_detailed_pos_calls "1 ENGI-MED" "ENGI-MEDs" "\t\t\t\t" "\"[Eng]\" {"
-		 		printf "\t\t\t}\n"
-		 		printf '\t\t\t"[Med/Snipe/Sp]" {\n'
-		 		gen_detailed_pos_calls "1 MED-MED" "MED-MEDs" "\t\t\t\t" "\"[Med]\" {"
-		 		gen_detailed_pos_calls "1 SNIPER-MED" "SNIPER-MEDs" "\t\t\t\t" "\"[Snipe]\" {"
-		 		gen_detailed_pos_calls "1 SPY-MED" "SPY-MEDs" "\t\t\t\t" "\"[Sp]\" {"
-		 		printf "\t\t\t}\n"
-		 		gen_detailed_pos_calls "1 MEDIC COMBO" "MEDIC COMBOS" "\t\t\t" "\"[Unknown]\" {"
-		 	else
-		 		printf "\t\t\"[%s]\" {\n" "$(printf '%s' "$Class")"
-		 		gen_detailed_pos_calls "1 $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]')" "$(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]')s" "\t\t\t"
-		 	fi
+		    # Generate position calls for all classes, except buildings.
+		    printf "\t\t\"[%s]\" {\n" "$Class"
+		    [ "$Class" != 'Med-Combo' ] && [ "$Class" != 'Builds' ] && gen_detailed_pos_calls "1 $(printf '%s' "${Class%%s}" | tr '[:lower:]' '[:upper:]')" "$(printf '%s' "${Class%%s}" | tr '[:lower:]' '[:upper:]')s" "\t\t\t"
 		 	# Create "has weapon" calls ONLY if the class actually has special weapons defined.
 		 	if [ "$(dasel select -f "$weapon_config" --null -s ".$Class")" != "null" ]
 		 	then
@@ -100,15 +77,15 @@ main() {
 		 			printf '\t\t\t\t"[%s]" {\n' "$WeaponSlot"
 		 			for CategoryIndex in $(dasel select -f "$weapon_config" -n -m -s ."$Class"."$WeaponSlot".- | tr -d '"')
 		 			do
-					    if [ -z "$(dasel select -f "$weapon_config" -n -s ."$Class"."$WeaponSlot".["$CategoryIndex"] | grep "weapons")" ]
+					    if [ -z "$(dasel select -f "$weapon_config" -n -s ."$Class"."$WeaponSlot".["$CategoryIndex"] | grep weapons)" ]
 					    then
-						Weapon="$(dasel select -f "$weapon_config" -s ."$Class"."$WeaponSlot".["$CategoryIndex"] | sed 's/- //'| tr -d '"')"
-					      	generate_cmenu_count "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]') USE $(printf '%s' "$Weapon" | tr '[:lower:]' '[:upper:]')!" "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]')S USE $(printf '%s' "$Weapon" | tr '[:lower:]' '[:upper:]')!" '\t\t\t\t\t' "[${Weapon}]"
+						Weapon="$(dasel select -f "$weapon_config" -s ."$Class"."$WeaponSlot".["$CategoryIndex"] | sed 's/- //' | tr -d '"')"
+					      	generate_cmenu_count "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]') USE $(printf '%b' "$Weapon" | tr '[:lower:]' '[:upper:]')!" "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]')S USE $(printf '%b' "$Weapon" | tr '[:lower:]' '[:upper:]')!" '\t\t\t\t\t' "[${Weapon}]"
 					    else
 					    	printf '\t\t\t\t\t"[%s]" {\n' "$(dasel select -f "$weapon_config" -n -s ."$Class"."$WeaponSlot".["$CategoryIndex"].category)"
-		 				for Weapon in $(dasel select -f "$weapon_config" -n -m -s ."$Class"."$WeaponSlot".["$CategoryIndex"].weapons | sed 's/- //' | sort)
+		 				for Weapon in $(dasel select -f "$weapon_config" -n -m -s ."$Class"."$WeaponSlot".["$CategoryIndex"].weapons | sed 's/- //' | tr -d '"')
 		 				do
-		 				    generate_cmenu_count "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]') USE $(printf '%s' "$Weapon" | tr '[:lower:]' '[:upper:]')!" "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]')S USE $(printf '%s' "$Weapon" | tr '[:lower:]' '[:upper:]')!" '\t\t\t\t\t\t' "[${Weapon}]"
+		 				    generate_cmenu_count "ENEMY $(printf '%s' "$Class" | tr '[:lower:]' '[:upper:]') USE $(printf '%b' "$Weapon" | tr '[:lower:]' '[:upper:]')!" "ENEMY $(printf '%b' "$Class" | tr '[:lower:]' '[:upper:]')S USE $(printf '%s' "$Weapon" | tr '[:lower:]' '[:upper:]')!" '\t\t\t\t\t\t' "[`printf '%b' "${Weapon}"`]"
 		 				done
 		 				printf '\t\t\t\t\t}\n'
 					    fi
@@ -117,60 +94,67 @@ main() {
 		 		done
 		 		printf '\t\t\t}\n'
 		 	fi
-		 	# Start Spy Disguises.
-		 	if [ "$Class" = "spy" ]
+			# Print out position calls for medic-X comboes.
+			if [ "$Class" = "Med-Combo" ]
 		 	then
-		 		printf "\t\t\t\"[Disguise]\" {\n"
-		 		for DisguiseGroup in 'scout soldier pyro' 'demo heavy engi' 'medic sniper spy'
+		 		printf '\t\t\t"[Scou/Solly/Pyr]" {\n'
+		 		gen_detailed_pos_calls "1 SCOUT-MED" "SCOUT-MEDs" "\t\t\t\t" "\"[Scou]\" {"
+		 		gen_detailed_pos_calls "1 SOLY-MED" "SOLY-MEDs" "\t\t\t\t" "\"[Solly]\" {"
+		 		gen_detailed_pos_calls "1 PYRO-MED" "PYRO-MEDs" "\t\t\t\t" "\"[Pyr]\" {"
+		 		printf '\t\t\t}\n'
+		 		printf '\t\t\t"[Dem/Heav/Eng]" {\n'
+		 		gen_detailed_pos_calls "1 DEMO-MED" "DEMO-MEDs" "\t\t\t\t" "\"[Dem]\" {"
+		 		gen_detailed_pos_calls "1 HEAVY-MED" "HEAVY-MEDs" "\t\t\t\t" "\"[Heav]\" {"
+		 		gen_detailed_pos_calls "1 ENGI-MED" "ENGI-MEDs" "\t\t\t\t" "\"[Eng]\" {"
+		 		printf '\t\t\t}\n'
+		 		printf '\t\t\t"[Med/Snipe/Sp]" {\n'
+		 		gen_detailed_pos_calls "1 MED-MED" "MED-MEDs" "\t\t\t\t" "\"[Med]\" {"
+		 		gen_detailed_pos_calls "1 SNIPR-MED" "SNIPR-MEDs" "\t\t\t\t" "\"[Snipe]\" {"
+		 		gen_detailed_pos_calls "1 SPY-MED" "SPY-MEDs" "\t\t\t\t" "\"[Sp]\" {"
+		 		printf '\t\t\t}\n'
+		 		gen_detailed_pos_calls "1 MED-COMBO" "MED-COMBOS" "\t\t\t" "\"[Unknown]\" {"
+		 	fi
+		 	# Start Spy Disguises.
+		 	if [ "$Class" = "Spy" ]
+		 	then
+		 		printf "\t\t\t\"[Disgis]\" {\n"
+		 		for DisguiseGroup in 'Scout Soly Pyro' 'Demo Heavy Engi' 'Medic Snipr Spy'
 		 		do
 		 			printf '\t\t\t\t"[%s]" {\n' "`printf '%s' "$DisguiseGroup" | tr '[:space:]' '/'`"
 		 			for DisguiseClass in $DisguiseGroup
 		 			do
 		 				generate_cmenu_count "SPY ${DisguiseClass}! (BIND)" "SPY ${DisguiseClass}s! (BIND)" '\t\t\t\t\t' "$Disguise"
 		 			done
-		 			printf "\t\t\t\t}\n"
+		 			printf '\t\t\t\t}\n'
 		 		done
-		 		printf "\t\t\t\tBIND \"Dead Rang!\" \"say_team SPY DEAD RANG (BIND)\" |\n"
-		 		printf "\t\t\t\tBIND \"Spy!\" \"say_team SPY - CHECK BACKS! (BIND)\" |\n"
-		 		printf "\t\t\t}\n"
+		 		printf '\t\t\t\tBIND "Ded Rang!" "say_team SPY DED RANG (BIND)" |\n'
+		 		printf '\t\t\t\tBIND "Spy!" "say_team SPY - CHEK BAKS! (BIND)" |\n'
+		 		printf '\t\t\t}\n'
 		 	fi
 		 	# ^ End Spy Disguises
 		 	# <Builds>
-		 	if [ "$ClassGroup" = 'builds' ]
+		 	if [ "$Class" = 'Builds' ]
 		 	then
-		 		for Build in "Sentry" "Dispenser" "Tele entrance" "Tele exit" "Nest"
+		 		for Build in "Sntry" "Dispenser" "Tele Ent." "Tele Exit" "Nest"
 		 		do
-		 			printf '\t\t\t"[%s]" {\n' "${Build}"
-		 			if [ "$Build" = "Sentry" ]
-		 			then
-		 				printf "\t\t\t\t\"[LvL1]\" {"
-		 				gen_detailed_pos_calls "1 L1 SENTRY" "L1 SENTRIES" "\t\t\t\t\t"
-		 				gen_detailed_pos_calls "DESTROYED 1 L1 SENTRY" "DESTROYED L1 SENTRIES" "\t\t\t\t\t" "\"[Destroyed]\" {"
-		 				printf "\t\t\t\t}"
-		 				printf "\t\t\t\t\"[LvL2]\" {"
-		 				gen_detailed_pos_calls "1 L2 SENTRY" "L2 SENTRIES" "\t\t\t\t\t"
-		 				gen_detailed_pos_calls "DESTROYED 1 L2 SENTRY" "DESTROYED L2 SENTRIES" "\t\t\t\t\t" "\"[Destroyed]\" {"
-		 				printf "\t\t\t\t}"
-		 				printf "\t\t\t\t\"[LvL3]\" {"
-		 				gen_detailed_pos_calls "1 L3 SENTRY" "L3 SENTRIES" "\t\t\t\t\t"
-		 				gen_detailed_pos_calls "DESTROYED 1 L3 SENTRY" "DESTROYED L3 SENTRIES" "\t\t\t\t\t" "\"[Destroyed]\" {"
-		 				printf "\t\t\t\t}"
-		 				printf "\t\t\t\t\"[Mini]\" {"
-		 				gen_detailed_pos_calls "1 L0 SENTRY" "L0 SENTRIES" "\t\t\t\t\t"
-		 				gen_detailed_pos_calls "DESTROYED 1 L0 SENTRY" "DESTROYED L0 SENTRIES" "\t\t\t\t\t" "\"[Destroyed]\" {"
-		 				printf "\t\t\t\t}"
-		 			else
-		 				# Generate position calls for buildings.
-		 				gen_detailed_pos_calls "1 $(printf "%s" "$Build" | tr "[:lower:]" "[:upper:]")" "$(printf "%s" "${Build}" | tr "[:lower:]" "[:upper:]")" "\t\t\t\t"
-		 				# and for destroyed ones.
-		 				gen_detailed_pos_calls "DESTROYED 1 $(printf "%s" "$Build" | tr "[:lower:]" "[:upper:]")" "DESTROYED $(printf "%s" "$Build" | tr "[:lower:]" "[:upper:]")s" "\t\t\t\t" "\"[Destroyed]\" {"
-		 			fi
-		 			printf "\t\t\t}\n"
+		 		    printf '\t\t\t"[%s]" {\n' "${Build}"
+				    # Generate position calls for buildings.
+		 		    gen_detailed_pos_calls "1 $(printf "%s" "$Build" | tr "[:lower:]" "[:upper:]")" "$(printf "%s" "${Build}" | tr "[:lower:]" "[:upper:]")" "\t\t\t\t" '"[General]" {'
+		 		    # and for destroyed ones.
+		 		    for Level in $([ "$Build" = "Sentry" ] && printf 'Mini') 'LvL1' 'LvL2' 'LvL3'
+				    do
+					printf '\t\t\t\t"[%s]" {\n' "$Level"
+					BuildNameCapitalized="$(printf '%s' "$Build" | tr '[:lower:]' '[:upper:]')"
+		 			gen_detailed_pos_calls "1 ENEMY $Level $BuildNameCapitalized" "ENEMY $Level ${BuildNameCapitalized}S" "\t\t\t\t\t"
+		 			gen_detailed_pos_calls "1 ENEMY $Level $BuildNameCaptialized KILLED" "ENEMY $Level ${BuildNameCapitalized}S KILD" "\t\t\t\t\t" '"[Kild]" {'
+		 			printf '\t\t\t\t}\n'
+				    done
+		 		    printf "\t\t\t}\n"
 		 		done
 		 	fi
 		 	# </Builds>
 			# <Phlog/GP calls>
-			if [ "$Class" = 'pyro' ]
+			if [ "$Class" = 'Pyro' ]
 			then
 				printf '\t\t\t"[Meters]" {\n'
 				for Meter in "Phlog" "Gas Passer"
@@ -181,10 +165,10 @@ main() {
 			fi
 			# </Phlog/GP calls>
 			# <Banner Calls>
-			if [ "$Class" = 'soldier' ]
+			if [ "$Class" = 'Soly' ]
 			then
 				printf '\t\t\t"[Banners]" {\n'
-				for Banner in "Buff Banner" "Battal" "Conch"
+				for Banner in "Buf Banr" "Batal" "Conch"
 				do
 				    generate_cmenu_count "1 ENEMY $(printf "%s" "$Banner" | tr "[:lower:]" "[:upper:]") USED!" "ENEMY $(printf "%s" "$Banner" | tr "[:lower:]" "[:upper:]")S USED!" "\t\t\t\t" "$Banner"
 				done
@@ -192,10 +176,10 @@ main() {
 			fi
 			# </Banner Calls>
 			# <Über calls>
-			if [ "$Class" = "medic" ]
+			if [ "$Class" = "Medic" ]
 			then
 				printf '\t\t\t"[Über]" {\n'
-				for UberType in "stock" "kritz" "quickie" "vacc" "unknown"
+				for UberType in "stok" "kritz" "quiki" "vacc" "unown"
 				do
 					printf '\t\t\t\t"[%s]" {\n' "$(printf '%s' "${UberType}")"
 					printf '\t\t\t\t\t"[Value]" {\n'
@@ -211,11 +195,9 @@ main() {
 					done
 					generate_cmenu_count "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBER @ 100%!" "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBERS @ 100%!" '\t\t\t\t\t\t' "100%"
 				      	printf "\t\t\t\t\t}\n"
-					for Verb in 'active' 'dropped' 'faked'
+					for Verb in 'popd' 'dropd' 'fakd'
 					do
-						printf "\t\t\t\t\t\"[%s]\" {\n" "$(printf '%s' "$Verb")"
-						generate_cmenu_count "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBER $(printf "%s" "$Verb" | tr "[:lower:]" "[:upper:]")" "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBERS $(printf "%s" "$Verb" | tr "[:lower:]" "[:upper:]")" '\t\t\t\t\t\t' "$UberType"
-						printf "\t\t\t\t\t}\n"
+						generate_cmenu_count "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBER $(printf "%s" "$Verb" | tr "[:lower:]" "[:upper:]")" "ENEMY $(printf "%s" "$UberType" | tr "[:lower:]" "[:upper:]") ÜBERS $(printf "%s" "$Verb" | tr "[:lower:]" "[:upper:]")" '\t\t\t\t\t' "$Verb"
 					done
 					printf "\t\t\t\t}\n"
 				done
